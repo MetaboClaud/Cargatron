ec01dcc15f1885aa6eee3fb7d35b270daf21ce0d
MetaboClaud <99832472+MetaboClaud@users.noreply.github.com>
2022-05-26 16:53:08 +0200

    Primera subida de aplicacion de streamlit

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import functions as ft
+
+
+st.set_page_config(layout='wide')
+
+pagina = st.sidebar.selectbox("Página", ('Home', 'Datos'))
+df = None
+
+if pagina == 'Home':
+    ft.home()
+elif pagina == 'Datos':
+    df= ft.datos()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.map(df)
+with col2:
+    df_bar = df.groupby('DISTRITO')['Nº CARGADORES'].sum()
+    st.bar_chart(df_bar)
+
+with col3:
+    df_bar = df.groupby('OPERADOR')['Nº CARGADORES'].sum()
+    st.bar_chart(df_bar)
